03c7158817739310508b785b8317b39b9687844a
unknown <vinodkumar9676150@gmail.com>
2023-02-21 15:25:28 +0530

    add sql files

---
+++ basics.sql
@@ -0,0 +1,12 @@
+-- create the new table using below comments
+CREATE TABLE student_mark(
+    stuend_name VARCHAR(250),
+    Mathematics int,
+    science int,
+    english int,
+    pass_percentage float
+    other text
+
+
+
+);
