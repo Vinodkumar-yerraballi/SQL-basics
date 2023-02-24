6b4c896cf0c1476103e325dad713508d25605548
unknown <vinodkumar9676150@gmail.com>
2023-02-24 21:04:11 +0530

    updates sql files

--- groupbyhaving.sql
+++ groupbyhaving.sql
@@ -53,3 +53,80 @@ ORDER BY
 
 -- WHERE vs HAVING: WHERE is used to filter rows and this operation is performed before grouping.
 -- HAVING is used to filter groups and this operation is performed after grouping.
+
+
+SELECT
+  name,
+  count(score) AS half_centuries
+FROM
+  player_match_details
+WHERE
+  score >= 50
+GROUP BY
+  name
+ORDER BY
+  half_centuries DESC;
+
+SELECT
+  name,
+  sum(fours) AS no_of_fours
+FROM
+  player_match_details
+GROUP BY
+  name
+ORDER BY
+  no_of_fours DESC;
+-- Get playernameand the total number of matches played asno_of_matchesby each player in the year 2012.
+
+-- Note: Output must contain rows in the descending order ofno_of_matchesof each player.
+
+SELECT
+  name,
+  count(MATCH) AS no_of_matches
+FROM
+  player_match_details
+WHERE
+  year = 2012
+GROUP BY
+  name
+ORDER BY
+  no_of_matches DESC;
+-- Get the year-wise performance, i.e., no_of_matchesand runs_scoredby each player.
+
+-- Note: Output must contain rows in the ascending order ofname&year
+
+-- Expected Output Format:
+-- -- 
+
+SELECT
+  name,
+  year,
+  count(MATCH) AS no_of_matches,
+  sum(score) AS runs_scored
+FROM
+  player_match_details
+GROUP BY
+  name,
+  year
+ORDER BY
+  name,
+  year ASC;
+
+-- For each player who scored more than 50 in at least 2 matches, get the total number of matches where the players scored more than 50.
+
+-- Note: Output must contain rows in the ascending order ofnameof the player.
+
+
+SELECT
+  name,
+  count(MATCH) AS no_of_matches
+FROM
+  player_match_details
+WHERE
+  score > 50
+GROUP BY
+  name
+HAVING
+  no_of_matches >= 2
+ORDER BY
+  name ASC;
