16ab86394262508e089e341d556a6e12ad4f8f2f
Vinodkumar Yerraballi <98636972+Vinodkumar-yerraballi@users.noreply.github.com>
2025-10-23 22:56:41 +0530

    Update Coding Question.sql

--- AdvanceSqlquries/Coding Question.sql
+++ AdvanceSqlquries/Coding Question.sql
@@ -8,3 +8,7 @@ select (max(salary)-(select max(salary) as engneer_salary
 from salary_max where department='engineering')) as salary_difference 
 from salary_max
 where department ='marketing';
+-- Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). Order the results by bikes that were most recently used.
+
+select bike_number, max(end_time) as last_used from dc_bikeshare_q1_2012
+group by bike_number ;
